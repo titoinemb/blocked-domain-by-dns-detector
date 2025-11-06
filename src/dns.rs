47b1096017fb05c3ce8e094e43803ee78e39cda6
Titoine MB <mb_titoine@proton.me>
2025-11-06 22:51:12 +0000

    Update dns.rs

--- src/dns.rs
+++ src/dns.rs
@@ -1,13 +1,11 @@
 // France
 pub const FR: &[&str] = &[
-  "8.8.8.8",
-  "8.8.4.4",
+
 ];
 
 // Angleterre
 pub const AN: &[&str] = &[
-  "1.1.1.1",
-  "1.0.0.1",
+
 ];
 
 // list of DNS by countries
